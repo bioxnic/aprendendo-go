e78d7c49e7e877368a934d094cd7bdfb3a49a840
Allan Presotto <allanpresottoifpr@gmail.com>
2018-08-08 17:17:07 -0300

    Scanf e ponteiro

--- src/hello/hello.go
+++ src/hello/hello.go
@@ -1,7 +1,6 @@
 package main
 
 import "fmt"
-import "reflect"
 
 func main() {
 	// tipos não precisam ser inferidos
@@ -9,9 +8,19 @@ func main() {
 	// Quando não inicializadas o valor atribuido é 0
 
 	nome := "allan"
-	idade := 18
+	// idade := 18
 	versao := 1.1
-	fmt.Println("Olá", nome, ", sua idade é", idade)
-	fmt.Println("Versão: ", versao)
-	fmt.Println("O tipo da variável nome é", reflect.TypeOf(nome))
+	fmt.Println("Olá", nome)
+	fmt.Println("Versão do programa: ", versao)
+
+	// fmt.Println("O tipo da variável nome é", reflect.TypeOf(nome))
+
+	fmt.Println("1 - Iniciar Monitoramento")
+	fmt.Println("2 - Exibir Logs")
+	fmt.Println("0 - Sair do Programa")
+
+	var comando int
+	fmt.Scanf("%d", &comando)
+	fmt.Println("O ponteiro/endereço da variável comando é", &comando)
+	fmt.Println("O comando escolhido foi", comando)
 }
