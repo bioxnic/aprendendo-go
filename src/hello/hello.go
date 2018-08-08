ac1a1e7a4b0e2372b40722de6185dbd81ef13b8d
Allan Presotto <allanpresottoifpr@gmail.com>
2018-08-08 17:23:25 -0300

    Sintaxe if/else e switch

--- src/hello/hello.go
+++ src/hello/hello.go
@@ -8,19 +8,39 @@ func main() {
 	// Quando não inicializadas o valor atribuido é 0
 
 	nome := "allan"
-	// idade := 18
 	versao := 1.1
 	fmt.Println("Olá", nome)
 	fmt.Println("Versão do programa: ", versao)
-
-	// fmt.Println("O tipo da variável nome é", reflect.TypeOf(nome))
 
 	fmt.Println("1 - Iniciar Monitoramento")
 	fmt.Println("2 - Exibir Logs")
 	fmt.Println("0 - Sair do Programa")
 
 	var comando int
+	// fmt.Scanf(&comando) // para inferir o tipo
 	fmt.Scanf("%d", &comando)
-	fmt.Println("O ponteiro/endereço da variável comando é", &comando)
 	fmt.Println("O comando escolhido foi", comando)
+
+	// if comando == 1 {
+	// 	fmt.Println("Monitorando...")
+	// } else if comando == 2 {
+	// 	fmt.Println("Exibindo logs...")
+	// } else if comando == 0 {
+	// 	fmt.Println("Saindo do programa...")
+	// } else {
+	// 	fmt.Println("Não conheço este comando:", comando)
+	// }
+
+	// Não precisa de breaks
+	switch comando {
+	case 1:
+		fmt.Println("Monitorando...")
+	case 2:
+		fmt.Println("Exibindo logs...")
+	case 0:
+		fmt.Println("Saindo do programa...")
+	default:
+		fmt.Println("Não conheço este comando:", comando)
+	}
+
 }
