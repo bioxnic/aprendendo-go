5dc7d5f43863bd61b30c3dbd8ebef40756377c6f
Allan Presotto <allan.p@pti.org.br>
2018-09-04 10:17:13 -0300

    Escrevendo o log, lendo o arquivo, formatando datas

--- src/hello/hello.go
+++ src/hello/hello.go
@@ -4,8 +4,10 @@ import (
 	"bufio"
 	"fmt"
 	"io"
+	"io/ioutil"
 	"net/http"
 	"os"
+	"strconv"
 	"strings"
 	"time"
 )
@@ -16,6 +18,7 @@ const delay = 5
 func main() {
 	// exibeNomes()
 	exibeIntroducao()
+	registraLog("site-false", false)
 	leSitesDoArquivo()
 
 	for {
@@ -43,6 +46,7 @@ func main() {
 			iniciarMonitoramento()
 		case 2:
 			fmt.Println("Exibindo logs...")
+			imprimeLogs()
 		case 0:
 			fmt.Println("Saindo do programa...")
 			os.Exit(0)
@@ -128,8 +132,10 @@ func testaSite(site string) {
 	}
 	if resp.StatusCode == 200 {
 		fmt.Println("Site:", site, "foi carregado com sucesso!")
+		registraLog(site, true)
 	} else {
 		fmt.Println("Site:", site, "está com problemas. Status code", resp.StatusCode)
+		registraLog(site, false)
 	}
 }
 
@@ -161,4 +167,25 @@ func leSitesDoArquivo() []string {
 
 	fmt.Println(sites)
 	return sites
+}
+
+func registraLog(site string, status bool) {
+	arquivo, err := os.OpenFile("log.txt", os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
+
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	arquivo.WriteString(time.Now().Format("02/01/2006 15:04:05") + "-" + site + "- online: " + strconv.FormatBool(status) + "\n")
+	arquivo.Close()
+}
+
+func imprimeLogs() {
+	arquivo, err := ioutil.ReadFile("log.txt")
+
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	fmt.Println(string(arquivo))
 }
